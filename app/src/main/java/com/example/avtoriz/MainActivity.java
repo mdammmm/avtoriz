e812268568046177f93e48b95d35acc95cabac0d
romanovaSmor <romanovasmor@iMac-Administrator-3.local>
2024-05-08 09:53:58 +0900

    Initial commit

---
+++ app/src/main/java/com/example/avtoriz/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.avtoriz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.content.Intent;
+import android.graphics.Color;
+//import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText username;
+    private EditText password;
+    private Button login;
+    private TextView loginLocked;
+    private TextView attempts;
+    private TextView numberOfAttempts;
+    int numberOfRemainingLoginAttempts = 3;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        username = (EditText) findViewById(R.id.edit_user);
+        password = (EditText) findViewById(R.id.edit_password);
+        login = (Button) findViewById(R.id.button_login);
+        loginLocked = (TextView) findViewById(R.id.login_Locked);
+        attempts = (TextView) findViewById(R.id.attempts);
+        numberOfAttempts = (TextView) findViewById(R.id.number_of_attempts);
+        numberOfAttempts.setText(Integer.toString(numberOfRemainingLoginAttempts));
+    }
+    public void Login(View view) {
+        if (username.getText().toString().equals("admin") &&
+                password.getText().toString().equals("admin")) {
+            Toast.makeText(getApplicationContext(), "Вход выполнен!",Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(MainActivity.this,Second.class);
+            startActivity(intent);
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "Неправильные данные!",Toast.LENGTH_SHORT).show();
+            numberOfRemainingLoginAttempts--;
+            attempts.setVisibility(View.VISIBLE);
+            numberOfAttempts.setVisibility(View.VISIBLE);
+            numberOfAttempts.setText(Integer.toString(numberOfRemainingLoginAttempts));
+            if (numberOfRemainingLoginAttempts == 0) {
+                login.setEnabled(false);
+                loginLocked.setVisibility(View.VISIBLE);
+                loginLocked.setBackgroundColor(Color.RED);
+                loginLocked.setText("Вход заблокирован!!!");
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/avtoriz/Second.java
@@ -0,0 +1,16 @@
+package com.example.avtoriz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.avtoriz.R;
+
+
+public class Second extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
